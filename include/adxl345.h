eb99f8d5d2b29bea4d0e40aab4b1fd1aa73a9377
Michael Kobit <mkobit@gmail.com>
2012-04-04 17:27:40 -0400

    Changed constant names of gyro and accel

--- include/adxl345.h
+++ include/adxl345.h
@@ -7,63 +7,65 @@
 #define ADXL_WRITE	0xA6
 
 //ADXL Register Map
-#define	DEVID			0x00	//Device ID Register
-#define THRESH_TAP		0x1D	//Tap Threshold
-#define	OFSX			0x1E	//X-axis offset
-#define	OFSY			0x1F	//Y-axis offset
-#define	OFSZ			0x20	//Z-axis offset
-#define	DUR				0x21	//Tap Duration
-#define	LATENT			0x22	//Tap latency
-#define	WINDOW			0x23	//Tap window
-#define	THRESH_ACT		0x24	//Activity Threshold
-#define	THRESH_INACT	0x25	//Inactivity Threshold
-#define	TIME_INACT		0x26	//Inactivity Time
-#define	ACT_INACT_CTL	0x27	//Axis enable control for activity and inactivity detection
-#define	THRESH_FF		0x28	//free-fall threshold
-#define	TIME_FF			0x29	//Free-Fall Time
-#define	TAP_AXES		0x2A	//Axis control for tap/double tap
-#define ACT_TAP_STATUS	0x2B	//Source of tap/double tap
-#define	BW_RATE			0x2C	//Data rate and power mode control
-#define POWER_CTL		0x2D	//Power Control Register
-#define	INT_ENABLE		0x2E	//Interrupt Enable Control
-#define	INT_MAP			0x2F	//Interrupt Mapping Control
-#define	INT_SOURCE		0x30	//Source of interrupts
-#define	DATA_FORMAT		0x31	//Data format control
-#define DATAX0			0x32	//X-Axis Data 0
-#define DATAX1			0x33	//X-Axis Data 1
-#define DATAY0			0x34	//Y-Axis Data 0
-#define DATAY1			0x35	//Y-Axis Data 1
-#define DATAZ0			0x36	//Z-Axis Data 0
-#define DATAZ1			0x37	//Z-Axis Data 1
-#define	FIFO_CTL		0x38	//FIFO control
-#define	FIFO_STATUS		0x39	//FIFO status
+#define	ADXL_DEVID			0x00	//Device ID Register
+#define ADXL_THRESH_TAP		0x1D	//Tap Threshold
+#define	ADXL_OFSX			0x1E	//X-axis offset
+#define	ADXL_OFSY			0x1F	//Y-axis offset
+#define	ADXL_OFSZ			0x20	//Z-axis offset
+#define	ADXL_DUR			0x21	//Tap Duration
+#define	ADXL_LATENT			0x22	//Tap latency
+#define	ADXL_WINDOW			0x23	//Tap window
+#define	ADXL_THRESH_ACT		0x24	//Activity Threshold
+#define	ADXL_THRESH_INACT	0x25	//Inactivity Threshold
+#define	ADXL_TIME_INACT		0x26	//Inactivity Time
+#define	ADXL_INACT_CTL		0x27	//Axis enable control for activity and inactivity detection
+#define	ADXL_THRESH_FF		0x28	//free-fall threshold
+#define	ADXL_TIME_FF		0x29	//Free-Fall Time
+#define	ADXL_TAP_AXES		0x2A	//Axis control for tap/double tap
+#define ADXL_ACT_TAP_STATUS	0x2B	//Source of tap/double tap
+#define	ADXL_BW_RATE		0x2C	//Data rate and power mode control
+#define ADXL_POWER_CTL		0x2D	//Power Control Register
+#define	ADXL_INT_ENABLE		0x2E	//Interrupt Enable Control
+#define	ADXL_INT_MAP		0x2F	//Interrupt Mapping Control
+#define	ADXL_INT_SOURCE		0x30	//Source of interrupts
+#define	ADXL_DATA_FORMAT	0x31	//Data format control
+#define ADXL_DATAX0			0x32	//X-Axis Data 0
+#define ADXL_DATAX1			0x33	//X-Axis Data 1
+#define ADXL_DATAY0			0x34	//Y-Axis Data 0
+#define ADXL_DATAY1			0x35	//Y-Axis Data 1
+#define ADXL_DATAZ0			0x36	//Z-Axis Data 0
+#define ADXL_DATAZ1			0x37	//Z-Axis Data 1
+#define	ADXL_FIFO_CTL		0x38	//FIFO control
+#define	ADXL_FIFO_STATUS	0x39	//FIFO status
 
 //Power Control Register Bits
-#define WU_0		(1<<0)	//Wake Up Mode - Bit 0
-#define	WU_1		(1<<1)	//Wake Up mode - Bit 1
-#define SLEEP		(1<<2)	//Sleep Mode
-#define	MEASURE		(1<<3)	//Measurement Mode
-#define AUTO_SLP	(1<<4)	//Auto Sleep Mode bit
-#define LINK		(1<<5)	//Link bit
+#define ADXL_WU_0		(1 << 0)	//Wake Up Mode - Bit 0
+#define	ADXL_WU_1		(1 << 1)	//Wake Up mode - Bit 1
+#define ADXL_SLEEP		(1 << 2)	//Sleep Mode
+#define	ADXL_MEASURE	(1 << 3)	//Measurement Mode
+#define ADXL_AUTO_SLP	(1 << 4)	//Auto Sleep Mode bit
+#define ADXL_LINK		(1 << 5)	//Link bit
 
 //Interrupt Enable/Interrupt Map/Interrupt Source Register Bits
-#define	OVERRUN		(1<<0)
-#define	WATERMARK	(1<<1)
-#define FREE_FALL	(1<<2)
-#define	INACTIVITY	(1<<3)
-#define	ACTIVITY	(1<<4)
-#define DOUBLE_TAP	(1<<5)
-#define	SINGLE_TAP	(1<<6)
-#define	DATA_READY	(1<<7)
+#define	ADXL_OVERRUN	(1 << 0)
+#define	ADXL_WATERMARK	(1 << 1)
+#define ADXL_FREE_FALL	(1 << 2)
+#define	ADXL_INACTIVITY	(1 << 3)
+#define	ADXL_ACTIVITY	(1 << 4)
+#define ADXL_DOUBLE_TAP	(1 << 5)
+#define	ADXL_SINGLE_TAP	(1 << 6)
+#define	ADXL_DATA_READY	(1 << 7)
 
 //Data Format Bits
-#define RANGE_0		(1<<0)
-#define	RANGE_1		(1<<1)
-#define JUSTIFY		(1<<2)
-#define	FULL_RES	(1<<3)
+#define ADXL_RANGE_0	(1 << 0)
+#define	ADXL_RANGE_1	(1 << 1)
+#define ADXL_JUSTIFY	(1 << 2)
+#define	ADXL_FULL_RES	(1 << 3)
 
-#define	INT_INVERT	(1<<5)
-#define	SPI			(1<<6)
-#define	SELF_TEST	(1<<7)
+#define	ADXL_INT_INVERT	(1 << 5)
+#define	ADXL_SPI		(1 << 6)
+#define	ADXL_SELF_TEST	(1 << 7)
+
+
 
 #endif
--- include/itg3200.h
+++ include/itg3200.h
@@ -19,7 +19,7 @@
 #define GYRO_YOUT_L	0x20
 #define GYRO_ZOUT_H	0x21
 #define GYRO_ZOUT_L	0x22
-#define	PWR_MGM		0x3E
+#define	ITG_PWR_MGM	0x3E
 
 //Sample Rate Divider
 //Fsample = Fint / (divider + 1) where Fint is either 1kHz or 8kHz
@@ -27,30 +27,30 @@
 //DLPF, Full Scale Register Bits
 //FS_SEL must be set to 3 for proper operation
 //Set DLPF_CFG to 3 for 1kHz Fint and 42 Hz Low Pass Filter
-#define DLPF_CFG_0	(1<<0)
-#define DLPF_CFG_1	(1<<1)
-#define DLPF_CFG_2	(1<<2)
-#define DLPF_FS_SEL_0	(1<<3)
-#define DLPF_FS_SEL_1	(1<<4)
+#define ITG_DLPF_CFG_0	(1 << 0)
+#define ITG_DLPF_CFG_1	(1 << 1)
+#define ITG_DLPF_CFG_2	(1 << 2)
+#define ITG_DLPF_FS_SEL_0	(1 << 3)
+#define ITG_DLPF_FS_SEL_1	(1 << 4)
 
 //Power Management Register Bits
 //Recommended to set CLK_SEL to 1,2 or 3 at startup for more stable clock
-#define PWR_MGM_CLK_SEL_0	(1<<0)
-#define PWR_MGM_CLK_SEL_1	(1<<1)
-#define PWR_MGM_CLK_SEL_2	(1<<2)
-#define PWR_MGM_STBY_Z	(1<<3)
-#define PWR_MGM_STBY_Y	(1<<4)
-#define PWR_MGM_STBY_X	(1<<5)
-#define PWR_MGM_SLEEP	(1<<6)
-#define PWR_MGM_H_RESET	(1<<7)
+#define ITG_PWR_MGM_CLK_SEL_0	(1 << 0)
+#define ITG_PWR_MGM_CLK_SEL_1	(1 << 1)
+#define ITG_PWR_MGM_CLK_SEL_2	(1 << 2)
+#define ITG_PWR_MGM_STBY_Z	(1 << 3)
+#define ITG_PWR_MGM_STBY_Y	(1 << 4)
+#define ITG_PWR_MGM_STBY_X	(1 << 5)
+#define ITG_PWR_MGM_SLEEP	(1 << 6)
+#define ITG_PWR_MGM_H_RESET	(1 << 7)
 
 //Interrupt Configuration Bits
-#define INT_CFG_ACTL	(1<<7)
-#define INT_CFG_OPEN	(1<<6)
-#define INT_CFG_LATCH_INT_EN	(1<<5)
-#define INT_CFG_INT_ANYRD	(1<<4)
-#define INT_CFG_ITG_RDY_EN	(1<<2)
-#define INT_CFG_RAW_RDY_EN	(1<<0)
+#define ITG_INT_CFG_ACTL			(1 << 7)
+#define ITG_INT_CFG_OPEN			(1 << 6)
+#define ITG_INT_CFG_LATCH_INT_EN	(1 << 5)
+#define ITG_INT_CFG_INT_ANYRD		(1 << 4)
+#define ITG_INT_CFG_ITG_RDY_EN		(1 << 2)
+#define ITG_INT_CFG_RAW_RDY_EN		(1 << 0)
 
 
 #endif

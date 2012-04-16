c1489694ca7cf2ed682cb4edcabfa02b208d0bba
Michael Kobit <mkobit@gmail.com>
2012-04-16 16:16:49 -0400

    Updated struture to represent changes in accel and gyro structures. Added several function declarations.

--- include/imu.h
+++ include/imu.h
@@ -4,10 +4,37 @@
 #include "itg3200.h"
 #include "adxl345.h"
 
-typedef struct imu {
+typedef enum {
+  IMU_SUCCESS = 0,
+  IMU_FAIL
+} IMU_RESULT;
+
+typedef struct imu_t {
   I2C_MODULE i2c_module;
-  accel_raw_readings accel;
-  gyro_raw_readings gyro;
-} imu_readings;
+  BOOL isOn;
+  accel_raw_t accel_raw;
+  gyro_raw_t gyro_raw;
+  BOOL updateAccelFirst;
+} imu_t;
+
+IMU_RESULT ImuInit(imu_t* imu, 
+					unsigned int peripheral_clock_speed, 
+					unsigned int i2c_speed, 
+					char accel_resolution, 
+					char accel_bandwidth, 
+					char gyro_dlpf_lpf, 
+					char gyro_sample_rate_div, 
+					char gyro_power_mgmt_sel);
+IMU_RESULT ImuUpdate(imu_t *imu);
+accel_raw_t *ImuGetRawAccel(imu_t *imu);
+gyro_raw_t *ImuGetRawGyro(imu_t *imu);
+BOOL ImuIsOn(imu_t *imu);
+double ImuGetGyroTemp(imu_t *imu);
+double ImuGetGyroX(imu_t *imu);
+double ImuGetGyroY(imu_t *imu);
+double ImuGetGyroZ(imu_t *imu);
+double ImuGetAccelX(imu_t *imu);
+double ImuGetAccelY(imu_t *imu);
+double ImuGetAccelZ(imu_t *imu);
 
 #endif

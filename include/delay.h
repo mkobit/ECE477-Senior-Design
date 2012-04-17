61e2ad4acddaec71658c4461611f06b39bb08764
Michael Kobit <mkobit@gmail.com>
2012-04-16 21:11:33 -0400

    Changed documentation headers to the new updated ones, also added a couple methods for determining how many micrseconds may have passed. Could be used by functions that want to know how much time has passed between two function calls which might be useful for our Kalman filter calculations

--- include/delay.h
+++ include/delay.h
@@ -4,9 +4,13 @@
 #define DELAY_US_TO_CT_TICKS(core_timer) (core_timer / 1000000UL)    // uS to core_timer ticks 
 #define DELAY_MS_TO_CT_TICKS(core_timer) (core_timer / 1000UL)
 
+typedef unsigned int us_t;
+
 void DelayInit(unsigned int clock_hz);
 void DelayS(unsigned int s);
 void DelayMs(unsigned int ms);
 void DelayUs(unsigned int us);
+us_t DelayUtilGetUs(void);
+us_t DelayUtilElapsedUs(us_t start, us_t end);
 
 #endif

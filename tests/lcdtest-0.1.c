d2b5a91167292db329c147cc0d31e54d3ef5594b
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-20 17:38:24 -0400

    Testing the LCD

--- tests/lcdtest-0.1.c
+++ tests/lcdtest-0.1.c
@@ -11,6 +11,28 @@
 #pragma config FPLLODIV = DIV_1
 
 #define SYSTEM_FREQUENCY 72000000L
+#define LCDS_IN 11
+
+
+typedef struct TEST_PAIR {
+    unsigned int bitnum;
+    IoPortId port_id;
+} TEST_PAIR;
+
+
+TEST_PAIR lcd_pairs[LCDS_IN] = {
+        {BIT_2, IOPORT_B}, \
+        {BIT_6, IOPORT_F}, \
+        {BIT_9, IOPORT_E}, \
+        {BIT_14, IOPORT_D}, \
+        {BIT_8, IOPORT_E}, \
+        {BIT_15, IOPORT_D}, \
+        {BIT_2, IOPORT_A}, \
+        {BIT_3, IOPORT_A}, \
+        {BIT_13, IOPORT_G}, \
+        {BIT_14, IOPORT_G}, \
+        {BIT_4, IOPORT_C}};
+
 int main(void)
 {
   int pbFreq;
@@ -18,28 +40,37 @@ int main(void)
   pbFreq = SYSTEMConfigPerformance(SYSTEM_FREQUENCY);
   DelayInit(SYSTEM_FREQUENCY);
   DelayS(1);  // simple delay to get ready for program
-  LcdInit(BIT_2, IOPORT_F,
-        BIT_3, IOPORT_F,
-        BIT_5, IOPORT_F,
-        BIT_3, IOPORT_G,
-        BIT_1, IOPORT_B,
-        BIT_4, IOPORT_B,
-        BIT_8, IOPORT_E,
-        BIT_15, IOPORT_D,
-        BIT_0, IOPORT_F,
-        BIT_1, IOPORT_F,
-        BIT_4, IOPORT_F,
-        LCD_INTERFACE_LENGTH_8BIT, LCD_LINES_2, LCD_DOTS_5x8);   // Using a wide selection of pins in lab with these ports connected
+  LcdInit(lcd_pairs[0].bitnum, lcd_pairs[0].port_id,
+        lcd_pairs[1].bitnum, lcd_pairs[1].port_id,
+        lcd_pairs[2].bitnum, lcd_pairs[2].port_id,
+        lcd_pairs[3].bitnum, lcd_pairs[3].port_id,
+        lcd_pairs[4].bitnum, lcd_pairs[4].port_id,
+        lcd_pairs[5].bitnum, lcd_pairs[5].port_id,
+        lcd_pairs[6].bitnum, lcd_pairs[6].port_id,
+        lcd_pairs[7].bitnum, lcd_pairs[7].port_id,
+        lcd_pairs[8].bitnum, lcd_pairs[8].port_id,
+        lcd_pairs[9].bitnum, lcd_pairs[9].port_id,
+        lcd_pairs[10].bitnum, lcd_pairs[10].port_id,
+        LCD_DOTS_5x8);   // Using a wide selection of pins in lab with these ports connected
   // this test will just test writing to the LCD
-  LcdInstrSetDisplayMode(LCD_DISPLAY_ON, LCD_CURSOR_OFF, LCD_CURSOR_BLINK_ON);;
+  LcdInstrSetDisplayMode(LCD_DISPLAY_ON, LCD_CURSOR_OFF, LCD_CURSOR_BLINK_OFF);
   
   while(1) {
-    LcdReturnHome();
+    LcdInstrReturnHome();
     LcdDisplayData("Hello\nThere!");
     DelayS(3);
     LcdInstrClearDisplay();
     DelayS(2);
-    LcdDisplayData("Just cleared\nDid you see that?");
+    LcdDisplayData("Just cleared it.\nYou see that?");
+    DelayS(2);
+    LcdInstrClearDisplay();
+    LcdDisplayData("Cleared it again but now to test a long string to check lines");
+    DelayS(2);
+    LcdInstrClearDisplay();
+    LcdDisplayData("Return H");
+    LcdInstrSetDDRAMAddress(14);
+    LcdDisplayData("END!");
+    DelayS(2);
   }
 
   return 0;

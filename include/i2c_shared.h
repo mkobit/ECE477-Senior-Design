a03daa949d749b5b5bca463fe74238eaa651d69b
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-23 15:55:11 -0400

    Changed all paramaters and other parts that are important to unsigned chars

--- include/i2c_shared.h
+++ include/i2c_shared.h
@@ -2,8 +2,9 @@
 #define I2C_SHARED_H
 
 BOOL I2CShared_Init(I2C_MODULE i2c, unsigned int peripheral_clock_speed, unsigned int i2c_speed);
-BOOL I2CShared_WriteByte(I2C_MODULE i2c, char i2c_addr, char i2c_register, BYTE data);
-BOOL I2CShared_ReadByte(I2C_MODULE i2c, char i2c_addr_write, char i2c_addr_read, char i2c_register, char *buffer);
-BOOL I2CShared_ReadMultipleBytes(I2C_MODULE i2c, char i2c_addr_write, char i2c_addr_read, char i2c_register_start, int nbytes, char *buffer);
+BOOL I2CShared_WriteByte(I2C_MODULE i2c, unsigned char i2c_addr, unsigned char i2c_register, unsigned char data);
+BOOL I2CShared_ReadByte(I2C_MODULE i2c, unsigned char i2c_addr_write, unsigned char i2c_addr_read, unsigned char i2c_register, unsigned char *buffer);
+BOOL I2CShared_ReadMultipleBytes(I2C_MODULE i2c, unsigned char i2c_addr_write, unsigned char i2c_addr_read, unsigned char i2c_register_start, int nbytes, unsigned char *buffer);
+inline void I2CShared_ResetBus(I2C_MODULE i2c);
 
 #endif

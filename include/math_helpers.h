098bc8c0b6a3a0e9924494d9033d2eb3776b5671
Michael Kobit <mkobit@gmail.com>
2012-04-24 02:37:17 -0400

    Added several helper functions for quaternions and euler angles, depending on what we need on the waist unit end

--- include/math_helpers.h
+++ include/math_helpers.h
@@ -5,6 +5,11 @@
 
 //void MHelpers_ConvertDegreesToRPYQuat(float roll, float pitch, float yaw, YAW_PITCH_ROLL_QUAT *quat);
 //void MHelpers_FindRotationAxis(YAW_PITCH_ROLL_QUAT *quat, ROTATION_AXIS *axis);
+
+void MHelpers_FillInEuler(const float roll, const float pitch, const float yaw, EULER_ANGLES *const e);
+void MHelpers_FillInQuat(const float q0, const float q1, const float q2, const float q3, QUATERNION *const q);
+void MHelpers_EulerToQuaternion(const EULER_ANGLES *const source, QUATERNION *const dest);
+void MHelpers_QuaternionToEuler(const QUATERNION *const source, EULER_ANGLES *const dest); 
 float MHelpers_FastInvSqrt(const float x);
 float MHelpers_DegreesToRadians(const float degrees);
 
--- src/math_helpers.c
+++ src/math_helpers.c
@@ -35,11 +35,219 @@ Euler angles and quats - http://forum.onlineconversion.com/showthread.php?t=5408
   axis->axis_z = cosf(quat->beta_z);
 }*/
 
+/************************************************************************************************** 
+  Function: 
+    void MHelpers_FillInEuler(const float roll, const float pitch, const float yaw, EULER_ANGLES *const e)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    fills in (e) Euler angles with roll, pitch, and yaw
+  
+  Description: 
+    Same as summary
+  
+  Preconditions: 
+    None
+  
+  Parameters: 
+    const float roll - roll angle, rotation about the X-axis
+		const float pitch - pitch angle, rotation about the Y-axis
+		const float yaw - yaw angle, rotation about the Z-axis
+		EULER_ANGLES *const e - Euler angles to be filled in
+  
+  Returns: 
+    void
+  
+  Example: 
+    <code>
+    MHelpers_FillInEuler(43.0f, 0.0f, 75.0f, &e)
+    </code>
+  
+  Conditions at Exit: 
+    (e) filled in with euler angles
+  
+**************************************************************************************************/
+void MHelpers_FillInEuler(const float roll, const float pitch, const float yaw, EULER_ANGLES *const e) {
+  e->roll = roll;
+  e->pitch = pitch;
+  e->yaw = yaw;
+}
 
+/************************************************************************************************** 
+  Function: 
+    void MHelpers_FillInQuat(const float q0, const float q1, const float q2, const float q3, QUATERNION *const q)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    Fills in quaternion (q) with values q0-q3
+  
+  Description: 
+    Same as summary
+  
+  Preconditions: 
+    None
+  
+  Parameters: 
+    const float q0 - quaternion value q0
+		const float q1 - quaternion value q1
+		const float q2 - quaternion value q2
+		const float q3 - quaternion value q3
+		QUATERNION *const q - quaternion to be filled in
+  
+  Returns: 
+    void
+  
+  Example: 
+    <code>
+    MHelpers_FillInQuat(1.0f, 0.0f, 0.0f, 0.0f, &q)
+    </code>
+  
+  Conditions at Exit: 
+    (q) quaternion filled in with values q0-q3
+  
+**************************************************************************************************/
+void MHelpers_FillInQuat(const float q0, const float q1, const float q2, const float q3, QUATERNION *const q) {
+  q->q0 = q0;
+  q->q1 = q1;
+  q->q2 = q2;
+  q->q3 = q3;
+}
 
+/************************************************************************************************** 
+  Function: 
+    void MHelpers_EulerToQuaternion(const EULER_ANGLES *const source, const QUATERNION *const dest)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    Converts the (source) Euler angles into the (dest) quaternion
+  
+  Description: 
+    Uses equations from Source: http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+  
+  Preconditions: 
+    (source) has vaild Euler angles
+  
+  Parameters: 
+    const EULER_ANGLES *const source - source Euler angles to use
+    const QUATERNION *const dest - destination quaternion to convert values into
+  
+  Returns: 
+    void
+  
+  Example: 
+    <code>
+    MHelpers_EulerToQuaternion(&euls, &quat)
+    </code>
+  
+  Conditions at Exit: 
+    (dest) has converted quaternion from the (source) Euler angles
+  
+**************************************************************************************************/
+void MHelpers_EulerToQuaternion(const EULER_ANGLES *const source, const QUATERNION *const dest) {
+  // local variables for readibility
+  const float roll = e->roll;
+  const float pitch = e->pitch;
+  const float yaw = e->yaw;
+  
+  // Local variables for faster conversion
+  const float cr = cosf(roll / 2);  // cos of roll
+  const float sr = sinf(roll / 2);  // sin of roll
+  const float cp = cosf(pitch / 2); // cos of pitch
+  const float sp = sinf(pitch / 2); // sin of pitch
+  const float cy = cosf(yaw / 2);   // cos of yaw
+  const float sy = sinf(yaw / 2);   // sin of yaw
+  
+  // calculations for quaternion conversion
+  dest->q0 = cr * cp * cy + sr * sp * sy;
+  dest->q1 = sr * cp * cy - cr * sp * sy;
+  dest->q2 = cr * sp * cy + sr * cp * sy;
+  dest->q3 = cr * cp * sy - sr * sp * cy;
+} 
+
+/************************************************************************************************** 
+  Function: 
+    void MHelpers_QuaternionToEuler(const QUATERNION *const source, EULER_ANGLES *const dest)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    Converts the (source) quaternion into the (dest) euler angles
+  
+  Description: 
+    Uses equations from Source: http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+  
+  Preconditions: 
+    (source) is a valid quaternion
+  
+  Parameters: 
+    const QUATERNION *const source - source qauternion to use
+    EULER_ANGLES *const dest - destination Euler angles to convert values into
+  
+  Returns: 
+    void
+  
+  Example: 
+    <code>
+    MHelpers_EulerToQuaternion(&quat, &euls)
+    </code>
+  
+  Conditions at Exit: 
+    (dest) has converted Euler angles from the (source) quaternion
+  
+**************************************************************************************************/
+void MHelpers_QuaternionToEuler(const QUATERNION *const source, EULER_ANGLES *const dest) {
+  // local variables for readibility
+  const float q0 = q->q0;
+  const float q1 = q->q1;
+  const float q2 = q->q2;
+  const float q3 = q->q3;
+  
+  // calculations for euler conversion
+  dest->roll = atan2f(2 * (q0 * q1 + q2* q3), 1 - 2 * (q1 * q1 + q2 * q2));
+  dest->pitch = arcsinf(2 * (q0 * q2 - q3 * q1));
+  dest->yaw = atan2f(2* (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3));
+} 
+
+/************************************************************************************************** 
+  Function: 
+    float MHelpers_FastInvSqrt(const float x)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    Returns 1/sqrt(x)
+  
+  Description: 
+    Uses formula from Source: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+  
+  Preconditions: 
+    None
+  
+  Parameters: 
+    const float x - number to find 1/sqrt of
+  
+  Returns: 
+    float y - 1/sqrt(x)
+  
+  Example: 
+    <code>
+    MHelpers_FastInvSqrt(9.0f)
+    </code>
+  
+  Conditions at Exit: 
+    None
+  
+**************************************************************************************************/
 float MHelpers_FastInvSqrt(const float x) {
-  // Source: http://en.wikipedia.org/wiki/Fast_inverse_square_root
-	float halfx = 0.5f * x;
+	const float halfx = 0.5f * x;
 	float y = x;
 	long i = *(long*)&y;
 	i = 0x5f3759df - (i>>1);
@@ -48,6 +256,37 @@ float MHelpers_FastInvSqrt(const float x) {
 	return y;
 }
 
+/************************************************************************************************** 
+  Function: 
+    float MHelpers_DegreesToRadians(const float degrees)
+  
+  Author(s): 
+    mkobit
+  
+  Summary: 
+    Converts degrees to radians
+  
+  Description: 
+    Use PI from math library and returns the radians in floats
+  
+  Preconditions: 
+    None
+  
+  Parameters: 
+    const float degrees - degrees to be converte
+  
+  Returns: 
+    float radians - radians converted from degrees
+  
+  Example: 
+    <code>
+    float rads = MHelpers_DegreesToRadians(85.0f)
+    </code>
+  
+  Conditions at Exit: 
+    None
+  
+**************************************************************************************************/
 float MHelpers_DegreesToRadians(const float degrees) {
   return degrees * (float) (PI / 180);
 }

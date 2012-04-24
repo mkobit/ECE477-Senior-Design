b80c8035388d9084cdd9750e8c8ae61946293b8d
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-24 14:46:35 -0400

    Added PI if it is not already defined

--- include/math_helpers.h
+++ include/math_helpers.h
@@ -3,6 +3,9 @@
 
 #include "kalman_shared.h"
 
+#ifndef PI
+#define PI 3.1415927f
+#endif
 //void MHelpers_ConvertDegreesToRPYQuat(float roll, float pitch, float yaw, YAW_PITCH_ROLL_QUAT *quat);
 //void MHelpers_FindRotationAxis(YAW_PITCH_ROLL_QUAT *quat, ROTATION_AXIS *axis);
 

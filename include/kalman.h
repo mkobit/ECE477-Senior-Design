ace4adcbe2d4287c30d0f3eeef2a59ec18c169b7
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-24 14:45:57 -0400

    Created seperate states for both types of Kalman filtering

--- include/kalman.h
+++ include/kalman.h
@@ -1,11 +1,27 @@
 #ifndef KALMAN_H
 #define KALMAN_H
 
+#include "kalman_shared.h"
 #include "imu.h"
 
-void Kalman_MadgwickUpdate(const imu_t *const p_imu, KALMAN_STATE *const q, const float sampleFreq);
-void Kalman_MahonyUpdate(const imu_t *const p_imu, KALMAN_STATE_MADGWICK *madgwick, const float sampleFreq)
 
-typedef QUATERNION KALMAN_STATE;
+// Madgwick filter variables
+typedef struct KALMAN_STATE_MADGWICK {
+  QUATERNION q;
+} KALMAN_STATE_MADGWICK;
+
+// Mahony filter variables
+typedef struct KALMAN_STATE_MAHONY {
+  QUATERNION q;
+  // integral error terms scaled by Ki
+  float integralFBx;
+  float integralFBy;
+  float integralFBz;
+}KALMAN_STATE_MAHONY;
+
+void Kalman_MadgwickInit(KALMAN_STATE_MADGWICK *const q);
+void Kalman_MadgwickUpdate(const imu_t *const p_imu, KALMAN_STATE_MADGWICK *const q, const float sampleFreq);
+void Kalman_MahonyInit(KALMAN_STATE_MAHONY *const q);
+void Kalman_MahonyUpdate(const imu_t *const p_imu, KALMAN_STATE_MAHONY *q, const float sampleFreq);
 
 #endif

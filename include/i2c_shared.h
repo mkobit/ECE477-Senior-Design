f7973dde77480c288a58e8334cef8a43d3d03d3e
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-24 13:29:25 -0400

    Added a possible fault limit that can be used when I2C fails during use

--- include/i2c_shared.h
+++ include/i2c_shared.h
@@ -1,6 +1,8 @@
 #ifndef I2C_SHARED_H
 #define I2C_SHARED_H
 
+#define I2CSHARED_FAULT_LIMIT 25000
+
 BOOL I2CShared_Init(const I2C_MODULE i2c, const UINT peripheral_clock_speed, const UINT i2c_speed);
 BOOL I2CShared_WriteByte(const I2C_MODULE i2c, const UINT8 i2c_addr, const UINT8 i2c_register, UINT8 data);
 BOOL I2CShared_ReadByte(const I2C_MODULE i2c, UINT8 i2c_addr_write, const UINT8 i2c_addr_read, const UINT8 i2c_register, UINT8 *const buffer);

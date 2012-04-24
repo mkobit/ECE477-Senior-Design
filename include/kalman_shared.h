cc0e23e40e6d7908118d688ca6dc7635a039697b
Michael Kobit <mkobit@gmail.com>
2012-04-24 02:37:43 -0400

    Changed typedef as well as adding constants for both kalman filtering

--- include/kalman_shared.h
+++ include/kalman_shared.h
@@ -1,19 +1,27 @@
 #ifndef KALMAN_SHARED_H
 #define KALMAN_SHARED_H
 
+// Used for Madgwick filtering
+#define BETADEF		0.1f		// 2 * proportional gain
+
+// Used for Mahony filtering
+#define TWOKPDEF	(2.0f * 0.5f)	// 2 * proportional gain
+#define TWOKIDEF	(2.0f * 0.0f)	// 2 * integral gain
+
+// Structure of euler angles that could possibly be used
 typedef struct EULER_ANGLES {
   float roll;   // rotation about the X-axis
   float pitch;  // rotation about the Y-axis
   float yaw;  // rotation about the Z-axis
 } EULER_ANGLES;
 
-// quaternion of sensor frame relative to auxiliary frame
-typedef struct SENSOR_FRAME_QUAT {
+// Quaternion of sensor frame relative to auxiliary frame
+typedef struct QUATERNION  {
   float q0;
   float q1;
   float q2;
   float q3;
-} YAW_PITCH_ROLL_QUAT;
+} QUATERNION;
 
 typedef struct ROTATION_AXIS {
   float axis_x;

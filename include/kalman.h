5cc6d553a0e3a66abe2c0b968f29b360328dfb8c
Michael Kobit <mkobit@gmail.com>
2012-04-23 20:26:02 -0400

    Added basic declarations for both the Madgwick and Mahony filtering

--- include/kalman.h
+++ include/kalman.h
@@ -1,6 +1,11 @@
 #ifndef KALMAN_H
 #define KALMAN_H
 
+#include "imu.h"
+
+void Kalman_MadgwickUpdate(imu_t *p_imu);
+void Kalman_MahonyUpdate(imu_t *p_imu);
+
 typedef struct kalman_state {
 } kalman_state;
 

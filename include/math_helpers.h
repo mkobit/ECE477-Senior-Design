a51eccc29b28b5daf40a34fd70e9c63680d1e5d1
Michael Kobit <mkobit@gmail.com>
2012-04-23 20:18:24 -0400

    Moved the structures to the "kalman_shared.h" file because I am trying to seperate what I can

--- include/math_helpers.h
+++ include/math_helpers.h
@@ -1,28 +1,11 @@
 #ifndef MATH_HELPERS_H
 #define MATH_HELPERS_H
 
-typedef struct EULER_ANGLES {
-  float roll;   // rotation about the X-axis
-  float pitch;  // rotation about the Y-axis
-  float yaw;  // rotation about the Z-axis
-} EULER_ANGLES;
+#include "kalman_shared.h"
 
-typedef struct YAW_PITCH_ROLL_QUAT {
-  float alpha;
-  float beta_x;
-  float beta_y;
-  float beta_z;
-} YAW_PITCH_ROLL_QUAT;
-
-typedef struct ROTATION_AXIS {
-  float axis_x;
-  float axis_y;
-  float axis_z;
-} ROTATION_AXIS;
-
-void Tracking_ConvertDegreesToRPYQuat(float roll, float pitch, float yaw, YAW_PITCH_ROLL_QUAT *quat);
-void Tracking_FindRotationAxis(YAW_PITCH_ROLL_QUAT *quat, ROTATION_AXIS *axis);
-float Tracking_FastInvSqrt(float x);
-float Tracking_DegreesToRadians(float degrees);
+void MHelpers_ConvertDegreesToRPYQuat(float roll, float pitch, float yaw, YAW_PITCH_ROLL_QUAT *quat);
+void MHelpers_FindRotationAxis(YAW_PITCH_ROLL_QUAT *quat, ROTATION_AXIS *axis);
+float MHelpers_FastInvSqrt(float x);
+float MHelpers_DegreesToRadians(float degrees);
 
 #endif

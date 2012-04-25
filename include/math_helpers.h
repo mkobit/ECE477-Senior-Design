ce9ee22e2686e4f7ca3ee43e981e17be71dc44a6
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-24 21:56:28 -0400

    Added RadsToDegs and also fixed a declaration

--- include/math_helpers.h
+++ include/math_helpers.h
@@ -11,11 +11,12 @@
 
 void MHelpers_FillInEuler(const float psi, const float theta, const float phi, EULER_ANGLES *const e);
 void MHelpers_FillInQuat(const float q0, const float q1, const float q2, const float q3, QUATERNION *const q);
-void MHelpers_FillInYPR(const float yaw, const float pitch, const float raw, YPR *const ypr);
+void MHelpers_FillInYPR(const float yaw, const float pitch, const float roll, YPR *const ypr);
 //void MHelpers_EulerToQuaternion(const EULER_ANGLES *const source, QUATERNION *const dest);
 void MHelpers_QuaternionToYPR(const QUATERNION *const source, YPR *const ypr);
 void MHelpers_QuaternionToEuler(const QUATERNION *const source, EULER_ANGLES *const dest); 
 float MHelpers_FastInvSqrt(const float x);
 float MHelpers_DegreesToRadians(const float degrees);
+float MHelpers_RadiansToDegrees(const float radians);
 
 #endif

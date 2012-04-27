5875b6e59e175b558c64a9b4a32cb13c23497181
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-27 15:49:28 -0400

    Changed #define-d and baudrate constant names as well as added parameters to the configure function

--- include/xbee.h
+++ include/xbee.h
@@ -1,26 +1,23 @@
 /*
- * File:   XBee.h
+ * File:   XBEE.h
  * Author: team7
  *
  * Created on April 25, 2012, 10:49 PM
  */
 
-#ifndef XBee_H
-#define XBee_H
+#ifndef XBEE_H
+#define XBEE_H
 
 //TODO: USER CODE
 
-#include <GenericTypeDefs.h>
-#include <plib.h>
-
-#define BaudRate 57600
+#define XBEE_BAUDRATE 57600
 #define Time3Period 3124
 #define CLEAR_VT "\033[2J"
 
 
 
 void XBeeSendDataBuffer(UART_MODULE,const char *buffer, UINT32 size);
-void XBeeConfigure(UART_MODULE , UINT32 , UINT32  );
+void XBeeConfigure(UART_MODULE , UINT32 freq, UINT32 baudrate);
 void XBeeCaptureSignalStrenth(void);
 VOID XBeeCaptureSignalStrenth2(void);
 

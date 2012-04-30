6f5ceab07910046a7956f7e206af2a3306b23e26
477grp7 <477grp7@shay.ecn.purdue.edu>
2012-04-30 14:53:24 -0400

    More testing on delay library after looking at problems

--- tests/delaytest-0.1.c
+++ tests/delaytest-0.1.c
@@ -1,8 +1,11 @@
 #include <plib.h>
-#include "delay.h"
 #include <p32xxxx.h>
-#include <string.h>
+#include <stdio.h>
+#include "delay.h"
+
+
 #include <stdlib.h>
+
 
 
 // TODO these need to be looked into to see what kind of config we need to use
@@ -14,11 +17,15 @@
 #pragma config FPBDIV = DIV_2
 #pragma config FPLLODIV = DIV_1*/
 
-#pragma config FPLLMUL = MUL_20, FPLLIDIV = DIV_2
-#pragma config FPLLODIV = DIV_1, FWDTEN = OFF
-#pragma config POSCMOD = HS, FNOSC = PRIPLL
+#pragma config FPLLMUL = MUL_20
+#pragma config FPLLIDIV = DIV_2
+#pragma config FPLLODIV = DIV_1
+#pragma config POSCMOD = HS
+#pragma config FNOSC = FRCPLL
+#pragma config FWDTEN = OFF // watchdog off
+#pragma config FPBDIV = DIV_1
 
-#define SYSTEM_FREQUENCY 80000000L
+#define SYSTEM_FREQUENCY 80000000UL
 #define BAUDRATE 57600
 #define CLEAR_VT "\033[2J"
 #define NEW_LINE_MODE "\033[20h"
@@ -57,44 +64,47 @@
     delay.c
     
   Update History: 
-    
+    4/30/12: Having problems testing with the peripheral bus on the PCB, need to figure out what is going on
+            * Found out that core timer does not use peripheral bus, it uses system clock
     
 **************************************************/
 int main(void)
 {
-	int nc;
-	int pbFreq;
-	long int delayed = 0;
-	char buffer[50] = "0";
+  int nc;
+  unsigned int pbFreq;
+  long int delayed = 0;
+  char buffer[50] = "0";
 
-	pbFreq = SYSTEMConfigPerformance(SYSTEM_FREQUENCY);
-	OpenUART2(UART_EN | UART_NO_PAR_8BIT | UART_1STOPBIT, UART_RX_ENABLE | UART_TX_ENABLE,
-		(pbFreq/16/BAUDRATE) - 1);
-	DelayInit(pbFreq);
-	putsUART2(CLEAR_VT);
-	DelayMs(1500);
-	putsUART2("Beginning in 3...\r\n");
-	DelayS(1);
-	putsUART2("Beginning in 2...\r\n");
-	DelayUs(50000);
-	DelayMs(995);
-	putsUART2("Beginning in 1...\n\r");
-	DelayS(1);
-	putsUART2("Start!\n\r");
-	while(1) {
-		DelayMs(770);
-		DelayS(2);
-		DelayUs(230000);
-		DelayMs(2000);
-		delayed += 5;
-		itoa(buffer, delayed, 10);
-		putsUART2(buffer);
-		putsUART2(" s\r\n");
-	}
+  pbFreq = SYSTEMConfigPerformance(SYSTEM_FREQUENCY);
+  //pbFreq = SYSTEM_FREQUENCY;
+  OpenUART2(UART_EN | UART_NO_PAR_8BIT | UART_1STOPBIT, UART_RX_ENABLE | UART_TX_ENABLE,
+          (pbFreq/16/BAUDRATE) - 1);
+  DelayInit(SYSTEM_FREQUENCY);
+  putsUART2(CLEAR_VT);
+  DelayMs(1500);
+  putsUART2("Beginning in 3...\r\n");
+  //printf("Hi\n");
+  DelayS(1);
+  putsUART2("Beginning in 2...\r\n");
+  DelayUs(50000);
+  DelayMs(995);
+  putsUART2("Beginning in 1...\n\r");
+  DelayS(1);
+  putsUART2("Start!\n\r");
+  while(1) {
+    DelayMs(770);
+    DelayS(2);
+    DelayUs(230000);
+    DelayMs(2000);
+    delayed += 5;
+    itoa(buffer, delayed, 10);
+    putsUART2(buffer);
+    putsUART2(" s\r\n");
+  }
 
-	//putsUART2(myHelloStr);
+  //putsUART2(myHelloStr);
 
-	return nc;
+  return nc;
 }
 
 /*void itoa(char *b, int n) {
